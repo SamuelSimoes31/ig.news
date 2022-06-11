0554b4f347f300a055acb525696139146d699758
Samuel Simões <samuelsimoes31@gmail.com>
2022-06-10 22:59:12 -0300

    feat: static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1L9IyqIzabFWmxYY9MkomTRH', {
     expand: ['product'] // para ter acesso a todas as informações do produto
   });
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24
   };
 };
