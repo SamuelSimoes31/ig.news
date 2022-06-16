edbb6d1d255c757926c96ebc8b6ed0e646742b97
Samuel Simões <samuelsimoes31@gmail.com>
2022-06-16 17:47:31 -0300

    feat: caving subscription on fauna db

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks: Buffer[] = [];
@@ -39,7 +40,21 @@ const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log(`Webhook received: ${event}`);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+            break;
+          default:
+            throw new Error(`Event type failed`);
+        }
+      } catch (error) {
+        res.json({ error: "Webhook handler failed" });
+      }
     }
 
     res.status(200).json({ message: "ok" });
