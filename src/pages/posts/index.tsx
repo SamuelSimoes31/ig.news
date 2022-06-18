2642fda512a9c36156da5b1497f1637551a5c502
Samuel Simões <samuelsimoes31@gmail.com>
2022-06-17 22:53:39 -0300

    feat: listing posts on posts screen

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,7 +2,17 @@ import Head from 'next/head';
 import { createClient } from '../../services/prismicio';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts } : PostProps) {
   return (
     <>
       <Head>
@@ -11,21 +21,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>17 de junho</time>
-            <strong>AAAAA</strong>
-            <p>dsgfsarhdesarhedrhedrthrtf</p>
-          </a>
-          <a href="">
-            <time>17 de junho</time>
-            <strong>AAAAA</strong>
-            <p>dsgfsarhdesarhedrhedrthrtf</p>
-          </a>
-          <a href="">
-            <time>17 de junho</time>
-            <strong>AAAAA</strong>
-            <p>dsgfsarhdesarhedrhedrthrtf</p>
-          </a>
+          {posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -35,9 +37,19 @@ export default function Posts() {
 export async function getStaticProps() {
   const client = createClient();
 
-  const posts = await client.getAllByType('posts');
+  const results = await client.getAllByType('posts');
+  // console.log(JSON.stringify(results, null, 2));
 
-  // console.log(JSON.stringify(posts, null, 2));
+  const posts = results.map(post => ({
+    slug: post.uid,
+    title: post.data.title[0].text,
+    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? "",
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }));
 
   return {
     props: { posts },
